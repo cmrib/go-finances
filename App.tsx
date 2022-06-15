cb83e0ad1a356de481a1cfa36c9ef8de15c75461
cmrib <ciceromirandar@gmail.com>
2022-06-15 16:02:32 -0300

    add styled-components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Welcome } from './src/components/Welcome';
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <Welcome title="Cicero Ribeiro" />
-    </View>
+    <Dashboard />
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
